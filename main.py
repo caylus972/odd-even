5bb6d82f68e55e323fa816b2a3862b2dcb984896
caylus972 <pamigeiadigwe@gmail.com>
2024-12-22 17:11:26 +0000

    second commit

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ if( n%2==0):
  print("the number is even")
 else:
  print("the number is odd")   
+
+
+i= int(input("enter a number :"))
+if (i<15):
+  print ("i is smaller than 15")
+  print("im if block")
+else:
+
+ print ("i is greater than 15")
+ print ("im in else block")
+print ("im not in if and not in else block")
